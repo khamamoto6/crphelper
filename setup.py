9559f1892a3c1c0790fac66ee16bcd5d4235f131
Kellyxx06 <41148452+Kellyxx06@users.noreply.github.com>
2021-08-07 03:22:06 +0900

    update requirements and setup

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
     version="0.0.1",
     author="Tomomi (Kelly) Hamamoto",
     author_email="khamamoto@hmc.edu",
-    description="A package to help analysis CRPropa simulations",
+    description="A package to help analyze CRPropa simulations",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/crphelper/homepage/",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
     ],
 )
